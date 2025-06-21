843e3ae0a0db7230439c5f919f91923a18634c40
eoin curtis <eoincur14@gmail.com>
2025-06-21 15:55:40 +0100

    prod

--- backend/app.py
+++ backend/app.py
@@ -84,5 +84,5 @@ def build_deck_route():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+ #   app.run(debug=False)
